4200da86800196cad09efb73f18421fc1cb07a0a
Rafael Pedroso <codeartisan@Dons-MacBook-Air.local>
2025-09-19 13:07:49 -0300

    fix: use correct translation keys for device drawer header
    - When editing: Show device name (e.g., 'iPhone 12')
    - When adding: Show 'Add Device' using sharedAdd + sharedDevice translation keys
    - Fixes missing 'Add Device' text in header

--- src/components/FloatingDevicesPopover.jsx
+++ src/components/FloatingDevicesPopover.jsx
@@ -682,7 +682,7 @@ const FloatingDevicesPopover = ({
                       <ChevronLeftIcon fontSize="small" />
                     </IconButton>
                     <Typography variant="h6" style={{ color: colors.text, fontWeight: '600', margin: 0 }}>
-                      {editingDevice?.id ? editingDevice.name : t('deviceAdd')}
+                      {editingDevice?.id ? editingDevice.name : `${t('sharedAdd')} ${t('sharedDevice')}`}
                     </Typography>
                   </div>
 
