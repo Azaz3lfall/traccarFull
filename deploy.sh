0ba5b960436ccc1f74b520dcc68e880991706793
Rafael Pedroso <codeartisan@Dons-MacBook-Air.local>
2025-10-03 21:12:54 -0300

    fix: Source nvm script directly for non-interactive SSH sessions
    - Change from 'source ~/.bashrc' to 'source ~/.nvm/nvm.sh'
    - This directly sources the nvm script instead of relying on bashrc
    - Should resolve 'nvm: command not found' error in SSH deployment
    - Ensures nvm is available in non-interactive shell sessions

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "mv /opt/traccar
 ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "ls /opt/traccar/web"
 
 # Install dependencies and manage resellersServer with PM2
-ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "nvm install v22.14.0"
-ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "nvm use v22.14.0"
-ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "cd /opt/traccar/web/addons/reseller && yarn"
+ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "source ~/.nvm/nvm.sh && nvm install v22.14.0"
+ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "source ~/.nvm/nvm.sh && nvm use v22.14.0"
+ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "source ~/.nvm/nvm.sh && cd /opt/traccar/web/addons/reseller && yarn"
 ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "pm2 stop resellersServer || true"
 ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "pm2 delete resellersServer || true"
 ssh -i /Users/codeartisan/github_vps_key root@codeartisan.cloud "cd /opt/traccar/web/addons/reseller && pm2 start /opt/traccar/web/addons/reseller/resellersServer.mjs --name resellersServer"
